ca727f3a665c1bd85d11d33bd8c4f2bf5605aa37
Eric Anderson <elanderson@gmail.com>
2018-06-06 07:00:16 -0500

    Update to ASP.NET Core 2.1 (#2)

--- AspNetCoreAngularAuth0/ApiApp/Startup.cs
+++ AspNetCoreAngularAuth0/ApiApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,7 +25,7 @@ namespace ApiApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddAuthentication(options =>
             {
